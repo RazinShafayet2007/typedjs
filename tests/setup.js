a5bc93912b99b7bd8acba9a8e6eebc7750bd8e1c
razinshafayet <sumonislamk29@gmail.com>
2026-01-31 22:20:12 +0600

    chore: add changes in setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -5,11 +5,11 @@
 if (process.env.DEBUG !== 'true') {
   global.console = {
     ...console,
-    log: jest.fn(),
-    debug: jest.fn(),
-    info: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn()
+    log: () => {},
+    debug: () => {},
+    info: () => {},
+    warn: () => {},
+    error: () => {}
   };
 }
 
